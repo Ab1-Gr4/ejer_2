a4f791b69c957f3dbef26ed87b035c44d8f81e7b
Ab1-Gr4 <enriquezabigail00@gmail.com>
2024-10-28 22:29:08 -0600

    sacar elementos en comun

--- main.py
+++ main.py
@@ -41,5 +41,6 @@ elementos2 = input("introduce numeros (separados por comas):")
 my_list2 = [int(num) for num in elementos2.split(',')]
 print("la lista 1 es:",my_list1)
 print("la lista 2 es:",my_list2)
-
+comun = [elem for elem in elementos1 if elem in elementos2 ]
+print("Elementos en comun:",comun)
 
