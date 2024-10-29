926479784662b6ccb66d56880af76416df7af9a5
Ab1-Gr4 <enriquezabigail00@gmail.com>
2024-10-28 21:29:39 -0600

    obtener el minimo

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def fibonacci(n):
     return res
 
 #min de una lista
-#prueba entrada de datos en una lista
+
 elementos = input("introduce numeros (separados por comas):")
 mi_lista = [int(num) for num in elementos.split(',')]
 print("la lista es:", mi_lista)
