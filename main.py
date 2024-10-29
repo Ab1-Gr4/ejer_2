93b885f601ed052f3a266d133e9e813843156efa
Ab1-Gr4 <enriquezabigail00@gmail.com>
2024-10-28 18:48:37 -0600

    prueba de impresion primeros casos

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 print("Ejercicio 2 secuencia de Fibonacci")
 num=input("introduce la cantidad de numeros de la secuencia deseas")
 print("numeros de la secuencia :",num)
-print("EjercIO")
+a=0
+b=1
+c=1
+print("La secuencia es:")
+print(" ",a,b)
